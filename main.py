e1f6978966992c309917b5ced4dafedee4be69dc
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2020-11-12 22:38:02 -0800

    Psuedocode

---
+++ main.py
@@ -0,0 +1,14 @@
+import fbchat, duolingo
+
+
+def main():
+    Auth into duolingo and facebook messenger
+    while True:
+        check if it is within some "check" timeframe
+        if it is, reload the duolingo auth and check whether mr kim did his shit
+        if he didnt, send a message
+        Check whether it is 9:00 on a saturday
+            rush in the regular crowd to ethan lu
+
+if __name__ == "__main__":
+    main()

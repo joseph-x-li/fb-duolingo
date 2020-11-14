69055e13474a7d8d5df09b997e14b3d8fcfa0a09
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2020-11-14 01:32:03 -0800

    Fix more grammar and wording

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Sincerely,
 The Duolingo Owl
 
 ![Owl threatening Konwoo](kimdead.png)
+
+<br><br><br><br><br>
+
+## What is this?
+
+Send a message over facebook messenger if Konwoo has not done his Duolingo for the day.
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logger = logging.getLogger("fb-duolingo")
 logger.setLevel(logging.INFO)
 logger.addHandler(filehandler)
 
-### see https://github.com/fbchat-dev/fbchat/issues/615#issuecomment-710127001 
+# see https://github.com/fbchat-dev/fbchat/issues/615#issuecomment-710127001 
 import re
 fbchat._util.USER_AGENTS    = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
 fbchat._state.FB_DTSG_REGEX = re.compile(r'"name":"fb_dtsg","value":"(.*?)"')
@@ -19,13 +19,13 @@ def main(patch=None):
     try:
         lingo = duolingo.Duolingo(duoname, duopass)
     except duolingo.DuolingoException as e:
-        logger.error(f"UNABLE TO SIGN INTO DUOLINGO: {e}")
+        logger.error(f"Unable to sign into Duolingo: {e}")
         return
     
     try:
         msnger = fbchat.Client(fbemail, fbpass, logging_level=logging.WARNING)
     except fbchat.FBchatException as e:
-        logger.error(f"UNABLE TO SIGN INTO FACEBOOK: {e}")
+        logger.error(f"Unable to sign into Facebook: {e}")
         return
 
     if patch is not None:
@@ -35,15 +35,16 @@ def main(patch=None):
         lingo.set_username(uname)
         info = lingo.get_streak_info()
         if info['streak_extended_today']:
+            logger.info(f"{uname} already did a lesson today. Yay!")
             continue
-        logger.info(f"{uname} did not do a lesson today... reminding")
+        logger.info(f"{uname} has not yet done a lesson today... reminding")
         message = f"Do a Duolingo lesson to save your {info['site_streak']} day streak"
         message += '!' * random.randint(1,6)
         msnger.send(fbchat.Message(text=message), thread_id=fbid)
         logger.info(f"Sent \"{message}\" to {uname}")
         msnger.send(fbchat.Message(sticker=fbchat.Sticker("237317987087861")), thread_id=fbid)
         
-    # Left out due to errors that make me have to reset my facebook password
+    # left out due to errors that make me have to reset my facebook password
     # msnger.logout()
     
 if __name__ == "__main__":

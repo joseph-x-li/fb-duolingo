6aad5ee2cb5896e903226e573c44e9c5b3f2df38
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2020-11-14 00:10:31 -0800

    MVP and funny image

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import fbchat
 import duolingo
 import logging
-from passwords import duoname, duopass, fbemail, fbpass
-
+import random
+from passwords import duoname, duopass, fbemail, fbpass, people
 logging.basicConfig(filename='events.log', level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
 
-duoppl = [
-    "KonwooKim",
-]
+### see https://github.com/fbchat-dev/fbchat/issues/615#issuecomment-710127001 
+import re
+fbchat._util.USER_AGENTS    = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
+fbchat._state.FB_DTSG_REGEX = re.compile(r'"name":"fb_dtsg","value":"(.*?)"')
 
-
-def main():
+def main(patch=None):
     try:
         lingo = duolingo.Duolingo(duoname, "duopass")
     except duolingo.DuolingoException as e:
         logging.error(f"UNABLE TO SIGN INTO DUOLINGO: {e}")
         return
+    
+    try:
+        msnger = fbchat.Client(fbemail, fbpass)
+    except fbchat.FBchatException as e:
+        logging.error(f"UNABLE TO SIGN INTO FACEBOOK: {e}")
+        return
 
-    # for person in duoppl:
-
-    # Auth into duolingo and facebook messenger
-    #     check if it is within some "check" timeframe
-    #     if it is, reload the duolingo auth and check whether mr kim did his shit
-    #     if he didnt, send a message
-    #     Check whether it is 9:00 on a saturday
-    #         rush in the regular crowd to ethan lu
-
-
+    if patch is not None:
+        people = [patch]
+    
+    for uname, fbid in people:
+        lingo.set_username(uname)
+        info = lingo.get_streak_info()
+        if info['streak_extended_today']:
+            continue
+        message = f"Do a lesson to save your {info['site_streak']} day streak"
+        message += '!' * random.randint(1,5)
+        msnger.send(fbchat.Message(text=message), thread_id=fbid)
+    
 if __name__ == "__main__":
     main()

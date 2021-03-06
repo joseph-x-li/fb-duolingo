4466003a4b56f4a22aa6754103439b1ca674bc6d
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2021-03-05 19:50:43 -0800

    small tweaks.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 import os
 import time
 from passwords import duoname, duopass, fbemail, fbpass, people
-filehandler = logging.FileHandler(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'events.log'))
+filehandler = logging.FileHandler(os.path.join(os.path.dirname(__file__), 'events.log'))
 filehandler.setFormatter(logging.Formatter(fmt="%(asctime)s:%(levelname)s:%(message)s"))
 logger = logging.getLogger("fb-duolingo")
 logger.setLevel(logging.INFO)
@@ -13,7 +13,8 @@ logger.addHandler(filehandler)
 
 # see https://github.com/fbchat-dev/fbchat/issues/615#issuecomment-710127001 
 import re
-fbchat._util.USER_AGENTS    = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
+# fbchat._util.USER_AGENTS    = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
+useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
 fbchat._state.FB_DTSG_REGEX = re.compile(r'"name":"fb_dtsg","value":"(.*?)"')
 
 def main(patch=None):
@@ -39,13 +40,14 @@ def main(patch=None):
             continue
         if msnger is None:
             try:
-                msnger = fbchat.Client(fbemail, fbpass, logging_level=logging.WARNING)
+                # msnger = fbchat.Client(fbemail, fbpass, logging_level=logging.WARNING)
+                msnger = fbchat.Client(fbemail, fbpass, user_agent=useragent, logging_level=logging.WARNING)
             except fbchat.FBchatException as e:
                 logger.error(f"Unable to sign into Facebook: {e}")
                 return
         logger.info(f"{uname} has not yet done a lesson today... reminding")
         message = f"Do a Duolingo lesson to save your {info['site_streak']} day streak"
-        message += '!' * random.randint(1,6)
+        message += '!' * random.randint(1, 6)
         msnger.send(fbchat.Message(text=message), thread_id=fbid)
         logger.info(f"Sent \"{message}\" to {uname}")
         msnger.send(fbchat.Message(sticker=fbchat.Sticker("237317987087861")), thread_id=fbid)
